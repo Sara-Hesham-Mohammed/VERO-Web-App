7f4e6d175b4c35809599e8b47521138de90b4fac
Sara-Hesham-Mohammed <142182302+Sara-Hesham-Mohammed@users.noreply.github.com>
2025-07-01 17:17:17 +0300

    providers

---
+++ app/Providers/RouteServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Route;
+
+
+class RouteServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     */
+
+    protected $namespace = "App\Http\Controllers";
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     */
+    public function boot(): void
+    {
+        parent::boot();
+        $this->mapRoutes();
+    }
+
+    public function mapRoutes(){
+        $this->mapWebRoutes();
+        $this->mapAdminRoutes();
+    }
+    protected function mapWebRoutes(){
+        Route::middleware("web")
+        ->namespace($this->namespace)
+        ->group(base_path("routes/web.php"));
+    }
+    protected function mapAdminRoutes(){
+        Route::namespace($this->namespace)
+        ->group(base_path("routes/admin.php"));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,34 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/welc', function () {
+    return ' welc';
+});
+
+Route::get('/withID/{id}', function ($id) {
+    return "welc $id";
+});
+
+
+Route::get('/doubleParam/{fname}/{lname}', function ($fname, $lname) {
+    return "welc $fname $lname";
+});
+
+Route::get('/profile/{id?}', function () {
+    return "welc, if no id passed it should work bardo 3ady";
+})->name('profile');
+
+
+// say ihave a bunch of routes related to user or a specific thing with subroutes
+
+Route::prefix("prof")->group(
+    function (){
+        
+        Route::get('/{id}', fn($id)=> "welcomeady")->where('id', '[0-9]+');
+
+        Route::get('/smth', function () {
+            return "welc this'll show the prof/smth endpoint";
+        });
+
+    });
